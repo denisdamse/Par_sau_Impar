5bc3efdb51daec36269c7df3b773238c491b5cdb
denisdamse <146414688+denisdamse@users.noreply.github.com>
2023-10-16 23:03:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Par_sau_Impar;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = Console.Read();
+        if (i % 2 == 0)
+        { Console.WriteLine("Par"); }
+        else Console.WriteLine("Impar");
+    }
+}
+
